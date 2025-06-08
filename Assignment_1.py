dd82fb164cbeb7db21bc0392c82ea283841536a7
Akshita Mishra <akshitamishra421@gmail.com>
2025-06-08 10:58:17 +0530

    Update Assignment_1.py

--- Assignment_1.py
+++ Assignment_1.py
@@ -3,19 +3,19 @@
 
 
 ###  Lower Triangle  ###
-n=int(input("Enter value of rows :"))
+n = int(input("Enter value of rows :"))
 for i in range(1,n+1):
   print('* '*i)
 
 
 ###  Upper Triangle  ###
-n=int(input("Enter value of rows :"))
+n = int(input("Enter value of rows :"))
 for i in range(n,0,-1):
   print('* '*i)
 
 
 ###  Pyramid  ###
-n=int(input("Enter value of rows :"))
+n = int(input("Enter value of rows :"))
 for i in range(1,n+1):
   print(' '*(n-i)+'* '*i)
 
