2c819ca3d4a44653ce39cf8ded22ccc6de89f8bb
Akshita Mishra <akshitamishra421@gmail.com>
2025-06-08 10:56:25 +0530

    updated_Assignment_1

--- Assignment_1.py
+++ Assignment_1.py
@@ -5,23 +5,17 @@
 ###  Lower Triangle  ###
 n=int(input("Enter value of rows :"))
 for i in range(1,n+1):
-  for j in range(1,i+1):
-    print('*',end=' ')
-  print()
+  print('* '*i)
+
 
 ###  Upper Triangle  ###
 n=int(input("Enter value of rows :"))
-for i in range(n):
-  for j in range(n,i,-1):
-    print('*',end=' ')
-  print()
+for i in range(n,0,-1):
+  print('* '*i)
+
 
 ###  Pyramid  ###
 n=int(input("Enter value of rows :"))
 for i in range(1,n+1):
-  for k in range(n-i):
-    print(' ',end='')
-  for j in range(i):
-    print('*',end=' ')
-  print()
+  print(' '*(n-i)+'* '*i)
 
